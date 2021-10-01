3839ecc4862782913da9db69ac610090f4dbbe57
Niklas Aaron Sherman Andersen <naaa@itu.dk>
2021-10-01 13:54:51 +0200

    Did something to the context

--- Assignment4.Entities/KanbanContext.cs
+++ Assignment4.Entities/KanbanContext.cs
@@ -1,6 +1,25 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+
 namespace Assignment4.Entities
 {
-    public class KanbanContext
+    public class KanbanContext : DbContext
     {
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public KanbanContext(DbContextOptions<KanbanContext> options) : base(options) { }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Task>()
+                .Property(e => e.State)
+                .HasConversion(
+                    v => v.ToString(),
+                    v => (State)Enum.Parse(typeof(State), v));
+        }
     }
 }
