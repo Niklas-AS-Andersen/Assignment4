7b0b1775a511f30766716c692ef1b8380ad291f5
Niklas Aaron Sherman Andersen <naaa@itu.dk>
2021-10-01 13:07:46 +0200

    first

--- Assignment4.Entities/Tag.cs
+++ Assignment4.Entities/Tag.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
 namespace Assignment4.Entities
 {
     public class Tag
     {
+        public int Id { get; set; }
+
+        [Key]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public List<Task> Tasks { get; set; }
     }
 }
