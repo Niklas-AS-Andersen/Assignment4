bb04f29dacbbf7858e8856171a5e2334dcf01fad
Niklas Aaron Sherman Andersen <naaa@itu.dk>
2021-10-04 10:06:54 +0200

    progress?

--- Assignment4.Entities/KanbanContext.cs
+++ Assignment4.Entities/KanbanContext.cs
@@ -6,7 +6,7 @@ namespace Assignment4.Entities
 {
     public class KanbanContext : DbContext
     {
-        const string conString = "postgres://sa:password@localhost:1433/kanban";
+        //const string conString = "Server=localhost;Database=Kanban;User Id=sa;Password=14a7372d-9ca3-468a-9b75-e2266bd96ebe";
         public DbSet<Task> Tasks { get; set; }
         public DbSet<Tag> Tags { get; set; }
         public DbSet<User> Users { get; set; }
--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Assignment4.Entities;
 
 namespace Assignment4
 {
@@ -6,7 +14,56 @@ namespace Assignment4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var configuration = LoadConfiguration();
+            var connectionString = configuration.GetConnectionString("Kanban");
+
+            var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>().UseSqlServer(connectionString);
+            using var context = new KanbanContext(optionsBuilder.Options);
+
+            var tag1 = new Tag 
+            {
+                Name = "SWU software",
+                Tasks = new List<Task>()
+            };
+
+            var user1 = new User
+            {
+                Name = "Paolo",
+                Email = "paolo@itu.dk",
+                Tasks = new List<Task>()
+            };
+
+            var task1 = new Task 
+            {
+                Title = "Assignment 4 SE",
+                AssignedTo = user1,
+                Description = "Write the SE part",
+                State = State.New,
+                Tags = new List<Tag>(){tag1}
+            };
+
+            tag1.Tasks.Add(task1);
+            user1.Tasks.Add(task1);
+
+            var tasks = from t in context.Tasks
+                        select new
+                        {
+                            t.Title
+                        };
+            foreach (var item in tasks)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>();
+
+            return builder.Build();
         }
     }
 }
