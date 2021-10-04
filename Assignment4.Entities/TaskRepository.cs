d1a981092201b26d224c2d79054ffb166e0d5d38
Niklas Aaron Sherman Andersen <naaa@itu.dk>
2021-10-04 14:44:01 +0200

    Started implementing TaskRepository
    Need to implement All() and FindById()

--- Assignment4.Entities/TaskRepository.cs
+++ Assignment4.Entities/TaskRepository.cs
@@ -1,6 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using Assignment4.Core;
+
 namespace Assignment4.Entities
 {
-    public class TaskRepository
+    public class TaskRepository: ITaskRepository
     {
+        private readonly SqlConnection _connection;
+
+        public TaskRepository(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IReadOnlyCollection<TaskDTO> All()
+        {
+            return null;
+        }
+
+        public int Create(TaskDTO task)
+        {
+            var cmdText = @"INSERT Task (Title, Description, AssignedToId)
+                            VALUES(@Title, @Description, @AssignedToId;
+                            SELECT SCOPE_IDENTITY()";
+            
+            using var command = new SqlCommand(cmdText, _connection);
+
+            command.Parameters.AddWithValue("@Title", task.Title);
+            command.Parameters.AddWithValue("@Description", task.Description);
+            command.Parameters.AddWithValue("@AssignedToId", task.AssignedToId);
+
+            OpenConnection();
+
+            var id = command.ExecuteScalar();
+
+            CloseConnection();
+
+            return (int)id;
+        }
+
+        public void Delete(int taskId)
+        {
+            var cmdText = @"DELETE Task WHERE Id = @Id";
+
+            using var command = new SqlCommand(cmdText, _connection);
+
+            command.Parameters.AddWithValue("@Id", taskId);
+
+            OpenConnection();
+
+            command.ExecuteNonQuery();
+
+            CloseConnection();
+        }
+
+        public TaskDetailsDTO FindById(int id)
+        {
+            return null;
+        }
+
+        public void Update(TaskDTO task)
+        {
+            var cmdText = @"UPDATE Task SET
+                            Title = @Title,
+                            Description = @Description,
+                            AssignedToId = @AssignedToId
+                            WHERE Id = @Id";
+
+            using var command = new SqlCommand(cmdText, _connection);
+
+            command.Parameters.AddWithValue("@Id", task.Id);
+            command.Parameters.AddWithValue("@Title", task.Title);
+            command.Parameters.AddWithValue("@Description", task.Description);
+            command.Parameters.AddWithValue("@AssignedToId", task.AssignedToId);
+
+            OpenConnection();
+
+            command.ExecuteNonQuery();
+
+            CloseConnection();
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+
+        private void OpenConnection()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+
+        private void CloseConnection()
+        {
+            if (_connection.State == ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
     }
 }
