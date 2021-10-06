d1cd595acad08f86ab15d6627f417fcee806d762
Leonoratheoret <70759304+Leonoratheoret@users.noreply.github.com>
2021-10-06 12:26:32 +0200

    problem in contexFactory, with the user

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -15,21 +15,29 @@ namespace Assignment4
         static void Main(string[] args)
         {
             var configuration = LoadConfiguration();
-            var connectionString = "Server=localhost;Database=Kanban;User Id=sa;Password=7b7249af-0e8d-444c-879b-555e94004d23"; //configuration.GetConnectionString("Kanban")
+            var connectionString = "Server=localhost;Database=Kanban;User Id=sa;Password=7b7249af-0e8d-444c-879b-555e94004d23"; //configuration.GetConnectionString("Kanban"); 
 
             var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>().UseSqlServer(connectionString);
             using var context = new KanbanContext(optionsBuilder.Options);
 
 
-            var users = from u in context.User
-                        select new
-                        {
-                            u.Name
-                        };
-            foreach (var item in users)
-            {
-                Console.WriteLine(item.Name);
-            }
+
+            // var Task1 = new Task {Title= "Make homework", AssignedTo = new User { Id = 2, Name= "Tessa", Email="Tessa@mail.dk", Tasks = new List<Task>()},
+            //    Description="this is not always a funny task", State = State.Active, Tags = new List<Tag>(){new Tag {Id = 1, Name = "food", Tasks = new List<Task>()}}};
+            // context.Task.Add(Task1);
+            // context.SaveChanges();
+
+            contextFactory.Seed(context);
+
+            // var users = from u in context.User
+            //             select new
+            //             {
+            //                 u.Name
+            //             };
+            // foreach (var item in users)
+            // {
+            //     Console.WriteLine(item.Name);
+            // }
         }
 
         static IConfiguration LoadConfiguration()
--- Assignment4/contexFactory.cs
+++ Assignment4/contexFactory.cs
@@ -3,8 +3,10 @@ using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
+using Assignment4.Entities;
+using System.Collections.Generic;
 
-namespace Assignment4.Entities{
+namespace Assignment4 {
  public class contextFactory : IDesignTimeDbContextFactory<KanbanContext>{
     public KanbanContext CreateDbContext(string[] args)
             {
@@ -21,5 +23,51 @@ namespace Assignment4.Entities{
 
                 return new KanbanContext(optionsBuilder.Options);
             }
+
+    public static void Seed(KanbanContext context){
+    context.Database.ExecuteSqlRaw("DELETE dbo.TagTask");
+    context.Database.ExecuteSqlRaw("DELETE dbo.Task");
+    context.Database.ExecuteSqlRaw("DELETE dbo.Tag");
+    context.Database.ExecuteSqlRaw("DELETE dbo.User");
+    context.Database.ExecuteSqlRaw("DBCC CHECKIDENT ('dbo.Task', RESEED, 0)");
+    context.Database.ExecuteSqlRaw("DBCC CHECKIDENT ('dbo.Tag', RESEED, 0)");
+    context.Database.ExecuteSqlRaw("DBCC CHECKIDENT ('dbo.User', RESEED, 0)");
+    
+
+    var user1 = new User {Name= "Tessa", Email="Tessa@mail.dk", Tasks = new List<Task>()};
+    var user2 = new User {Name= "Leonora", Email="Leonora@mail.dk", Tasks = new List<Task>()};
+    var user3 = new User { Name= "Frida", Email="Frida@mail.dk", Tasks = new List<Task>()};
+
+    var foodTag = new Tag {Name = "food", Tasks = new List<Task>()};
+    var SchoolTag = new Tag { Name = "School", Tasks = new List<Task>()};
+    var FreeTimeTag = new Tag {Name = "Free Time", Tasks = new List<Task>()};
+
+
+    /*var Task1 = new Task { Id = 1, Title= "Make homework", AssignedTo = user1,
+               Description="this is not always a funny task", State = State.Active, Tags = new List<Tag>(){foodTag}}
+    //var Task2 = new Task { Id = 2, Title= "Go for a run", AssignedTo = user3,
+               Description="this is soooo funny", State = State.Active, Tags = new List<Tag>(){FreeTimeTag}};
+    var Task3 = new Task { Id = 3, Title= "Eat a cupcake",
+               Description="mums", State = State.Resolved, Tags = new List<Tag>(){foodTag,FreeTimeTag}};*/
+    
+    // foodTag.Tasks.Add(Task3);
+    // SchoolTag.Tasks.Add(Task1);
+    // FreeTimeTag.Tasks.AddRange(new List<Task>(){Task2, Task3});
+
+    // user1.Tasks.Add(Task1);
+    // user3.Tasks.Add(Task2);
+
+   
+    context.Task.AddRange(
+        new Task {Title= "Go for a run", AssignedTo = user3,
+               Description="this is soooo funny", State = State.Active, Tags = new List<Tag>(){FreeTimeTag}},
+        new Task {Title= "Eat a cupcake",
+               Description="mums", State = State.Resolved, Tags = new List<Tag>(){foodTag,FreeTimeTag}},
+        new Task {Title= "Make homework", AssignedTo = user1,
+               Description="this is not always a funny task", State = State.Active, Tags = new List<Tag>(){foodTag}}
+    );
+
+    context.SaveChanges();
+}
  }
 }
